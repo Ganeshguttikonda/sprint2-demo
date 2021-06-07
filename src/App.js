7c93697f57e4301c7a58940c8034aaea11faa0a6
ganesh <guttikondaganesh26@gmail.com>
2021-06-07 17:52:26 +0530

    Routing done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import logo from "./logo.svg";
-import "./App.css";
-
-import { useState } from "react";
-import { Button, ButtonGroup, Col, Row } from "react-bootstrap";
-import { Member3, Registration } from "./components/Registration";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import { Registration } from "./components/Registration";
+import { Login } from "./components/Login";
 
-// TEAM LEADER
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact>
+        <Route path="/registration">
           <Registration />
+        </Route>
+
+        <Route path="/login">
+          <Login />
+        </Route>
+
+        <Route exact path="/">
+          <Login />
         </Route>
       </Switch>
     </Router>
--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,71 +1,7 @@
-import { Alert } from "react-bootstrap";
-
 export function Registration() {
   return (
-    <div className="w-75">
-      <div className="alert alert-primary">
-        <h3>Regitration Module</h3>
-      </div>
-
-      <div>
-        <div className="mb-1">
-          <input
-            type="text"
-            name=""
-            id=""
-            placeholder="Enter Firstname"
-            className="form-control"
-          />
-        </div>
-
-        <div className="mb-1">
-          <input
-            type="text"
-            name=""
-            id=""
-            placeholder="Enter Lastname"
-            className="form-control"
-          />
-        </div>
-
-        <div className="mb-1">
-          <input
-            type="text"
-            name=""
-            id=""
-            placeholder="Enter Password"
-            className="form-control"
-          />
-        </div>
-
-        <div className="mb-1">
-          <input
-            type="text"
-            name=""
-            id=""
-            placeholder="Enter Email"
-            className="form-control"
-          />
-        </div>
-
-        <div className="mb-1">
-          <input
-            type="text"
-            name=""
-            id=""
-            placeholder="Enter Mobile"
-            className="form-control"
-          />
-        </div>
-
-        <div className="mb-1">
-          <input
-            type="button"
-            value="Register"
-            className="btn btn-primary w-100"
-          />
-        </div>
-      </div>
+    <div>
+      <h1>Registration</h1>
     </div>
   );
 }
